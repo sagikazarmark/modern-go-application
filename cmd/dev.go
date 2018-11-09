67406746446001c0f7f53d7634d2bdb1d0c81311
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-11-09 11:33:16 +0100

    Remove environment variable running

--- cmd/dev.go
+++ cmd/dev.go
@@ -3,24 +3,10 @@
 package main
 
 import (
-	"path"
-	"runtime"
 	"time"
-
-	"github.com/joho/godotenv"
 )
 
 func init() {
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		panic("cannot get current dir: no caller information")
-	}
-
-	projectRoot := path.Clean(path.Dir(path.Dir(filename)))
-
-	godotenv.Load(path.Join(projectRoot, ".env"))
-	godotenv.Load(path.Join(projectRoot, ".env.dist"))
-
 	// Load defaults for info variables
 	if Version == "" {
 		Version = "dev"
