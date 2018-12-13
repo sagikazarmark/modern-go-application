9132c45ea67f1265b3b2b5322cffcbd09778ec42
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-13 17:12:05 +0100

    Update package name in examples

--- .init/init.sh
+++ .init/init.sh
@@ -14,15 +14,18 @@ mv .idea/project.iml .idea/${project}.iml
 rep "s|.idea/project.iml|.idea/${project}.iml|g" .idea/modules.xml
 
 # Run configurations
+rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/All_tests.xml
 rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Debug.xml
-rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/All.xml
-rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Unit.xml
-rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Acceptance.xml
-rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Integration.xml
+rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Integration_tests.xml
+rep "s|name=\"project\"|name=\"${project}\"|" .idea/runConfigurations/Tests.xml
 
+# Other project files
 rep "s|${boilerplatePackage}|${package}|" .circleci/config.yml
 rep "s|${boilerplatePackage}|${package}|" .gitlab-ci.yml
 rep "s|${boilerplatePackage}|${package}|" cmd/config.go
 rep "s|${boilerplatePackage}|${package}|" cmd/main.go
 rep "s|${boilerplatePackage}|${package}|" CHANGELOG.md
 rep "s|${boilerplatePackage}|${package}|" Dockerfile
+
+# Example code
+find internal/greeting -type f | while read file; do rep "s|${boilerplatePackage}|${package}|" "$file"; done
