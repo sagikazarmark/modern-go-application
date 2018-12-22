8ad228f6a8fe32bf77ca8fc8fe15cd574286c3fd
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-22 01:18:47 +0100

    Add component to prometheus exporter error handler

--- internal/platform/prometheus/prometheus.go
+++ internal/platform/prometheus/prometheus.go
@@ -10,7 +10,7 @@ import (
 func NewExporter(config Config, errorHandler emperror.Handler) (*prometheus.Exporter, error) {
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Namespace: config.Namespace,
-		OnError:   errorHandler.Handle,
+		OnError:   emperror.HandlerWith(errorHandler, "component", "metrics", "exporter", "prometheus").Handle,
 	})
 
 	return exporter, errors.Wrap(err, "failed to create prometheus exporter")
