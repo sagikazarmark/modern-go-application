6786a66e01a74dbe9c0a8cfaf3978709ae66f4b4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-01-26 18:02:04 +0100

    Add go-kit like decode-encode functions to http transport

--- internal/greeting/greetingdriver/http_controller.go
+++ internal/greeting/greetingdriver/http_controller.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/goph/emperror"
 	"github.com/moogar0880/problems"
+	"github.com/pkg/errors"
 
 	api "github.com/sagikazarmark/modern-go-application/.gen/openapi/greeting/go"
 	"github.com/sagikazarmark/modern-go-application/internal/greeting"
@@ -28,42 +29,66 @@ func NewHTTPController(helloService Greeter, errorHandler emperror.Handler) *HTT
 
 // SayHello says hello to someone.
 func (c *HTTPController) SayHello(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	var apiReq api.HelloRequest
-
-	if err := decoder.Decode(&apiReq); err != nil {
+	req, err := decodeHTTPSayHelloRequest(r)
+	if err != nil {
 		c.errorHandler.Handle(err)
 
-		http.Error(w, "invalid request", http.StatusBadRequest)
-
-		return
-	}
-
-	req := greeting.HelloRequest{
-		Name: apiReq.Name,
-	}
-
-	resp, err := c.helloService.SayHello(r.Context(), req)
-	if err != nil {
-		problem := problems.NewDetailedProblem(400, err.Error())
-
-		w.Header().Set("Content-Type", problems.ProblemMediaType)
-		err = json.NewEncoder(w).Encode(problem)
+		err := encodeHTTPErrorWithCode(err, http.StatusBadRequest, w)
 		if err != nil {
-			c.errorHandler.Handle(emperror.WrapWith(err, "failed to respond with error", "error", problem.Detail))
+			c.errorHandler.Handle(err)
 		}
 
 		return
 	}
 
+	resp, err := c.helloService.SayHello(r.Context(), req)
+	if err != nil {
+		err := encodeHTTPErrorWithCode(err, http.StatusBadRequest, w)
+		if err != nil {
+			c.errorHandler.Handle(err)
+		}
+
+		return
+	}
+
+	err = encodeHTTPSayHelloResponse(w, resp)
+	if err != nil {
+		c.errorHandler.Handle(err)
+	}
+}
+
+func decodeHTTPSayHelloRequest(r *http.Request) (greeting.HelloRequest, error) {
+	var req greeting.HelloRequest
+
+	var apiReq api.HelloRequest
+	err := json.NewDecoder(r.Body).Decode(&apiReq)
+	if err != nil {
+		return req, errors.Wrap(err, "failed to decode request")
+	}
+
+	req = greeting.HelloRequest{
+		Name: apiReq.Name,
+	}
+
+	return req, nil
+}
+
+func encodeHTTPSayHelloResponse(w http.ResponseWriter, resp *greeting.HelloResponse) error {
 	apiResp := api.HelloResponse{
 		Reply: resp.Reply,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(apiResp)
-	if err != nil {
-		c.errorHandler.Handle(emperror.WrapWith(err, "failed to send response"))
-	}
+	err := json.NewEncoder(w).Encode(apiResp)
+
+	return errors.Wrap(err, "failed to send response")
+}
+
+func encodeHTTPErrorWithCode(err error, statusCode int, w http.ResponseWriter) error {
+	problem := problems.NewDetailedProblem(statusCode, err.Error())
+
+	w.Header().Set("Content-Type", problems.ProblemMediaType)
+	err = json.NewEncoder(w).Encode(problem)
+
+	return emperror.WrapWith(err, "failed to respond with error", "error", problem.Detail)
 }
