198fc90766efb28755de1a93b6e8c623ed13bdfd
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-01-30 18:18:28 +0100

    Fix jaeger configuration

--- cmd/modern-go-application/config.go
+++ cmd/modern-go-application/config.go
@@ -151,7 +151,7 @@ func Configure(v *viper.Viper, p *pflag.FlagSet) {
 
 	v.SetDefault("instrumentation.prometheus.enabled", false)
 	v.SetDefault("instrumentation.jaeger.enabled", false)
-	v.SetDefault("instrumentation.jaeger.collectorendpoint", "http://localhost:14268/api/traces?format=jaeger.thrift")
+	v.SetDefault("instrumentation.jaeger.collectorEndpoint", "http://localhost:14268/api/traces?format=jaeger.thrift")
 	v.SetDefault("instrumentation.jaeger.agentEndpoint", "localhost:6831")
 	v.RegisterAlias("instrumentation.jaeger.serviceName", "serviceName")
 	_ = v.BindEnv("instrumentation.jaeger.username")
