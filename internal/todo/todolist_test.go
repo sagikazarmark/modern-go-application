2ee3471d7641cb48be030b64af78962faee22625
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-03-02 15:55:17 +0100

    Add tests for failing storage

--- internal/todo/todolist_test.go
+++ internal/todo/todolist_test.go
@@ -38,6 +38,17 @@ func TestTodoList_CreatesATodo(t *testing.T) {
 	assert.Equal(t, expectedTodo, todo)
 }
 
+func TestTodoList_CannotCreateATodo(t *testing.T) {
+	todoList := NewTodoList(idgen.NewConstantGenerator("id"), NewReadOnlyTodoStore(NewInmemoryTodoStore()))
+
+	req := CreateTodoRequest{
+		Text: "My first todo",
+	}
+
+	_, err := todoList.CreateTodo(context.Background(), req)
+	require.Error(t, err)
+}
+
 func TestTodoList_ListTodos(t *testing.T) {
 	todoStore := NewInmemoryTodoStore()
 
@@ -106,4 +117,23 @@ func TestTodoList_CannotMarkANonExistingTodoDone(t *testing.T) {
 
 	e := cause.(TodoNotFoundError)
 	assert.Equal(t, "id", e.ID)
+}
+
+func TestTodoList_StoringDoneTodoFails(t *testing.T) {
+	inmemTodoStore := NewInmemoryTodoStore()
+
+	todo := Todo{
+		ID:   "id",
+		Text: "Do me",
+	}
+	require.NoError(t, inmemTodoStore.Store(context.Background(), todo))
+
+	todoList := NewTodoList(nil, NewReadOnlyTodoStore(inmemTodoStore))
+
+	req := MarkAsDoneRequest{
+		ID: "id",
+	}
+
+	err := todoList.MarkAsDone(context.Background(), req)
+	require.Error(t, err)
 }
