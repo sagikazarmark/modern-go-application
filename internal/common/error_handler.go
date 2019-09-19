b6dbc64bc5b04c27440c4e7b9503d89a7b3e290d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-09-19 19:32:45 +0200

    Add noop error handler implementation

--- internal/common/error_handler.go
+++ internal/common/error_handler.go
@@ -9,3 +9,10 @@ type ErrorHandler interface {
 	// Handle handles an error.
 	Handle(ctx context.Context, err error)
 }
+
+type noopErrorHandler struct{}
+
+// NewNoopErrorHandler returns an error handler that discards every error.
+func NewNoopErrorHandler() ErrorHandler { return noopErrorHandler{} }
+
+func (noopErrorHandler) Handle(ctx context.Context, err error) {}
